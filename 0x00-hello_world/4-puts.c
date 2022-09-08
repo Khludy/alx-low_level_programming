102e8fc1f6ab9948e0abbe844730f09a5ee51b21
khludy <khludy3@gmail.com>
2022-09-08 11:26:12 -0700

    Task 2-8

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,16 @@
-#!/bin/bash
 #include <stdio.h>
 
 /**
+ * main - print the string in the put function
  *
- *  * main - main block
- *
- *   * Return: 0
- *
- *    */
-
+ * Description: using the main function
+ * this program prints "programming is like building a multilingual puzzle
+ * return: 0
+ */
 int main(void)
-
 {
-
 		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
+		return (0);
 
 }
 
