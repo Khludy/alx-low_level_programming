79dc490b0ad43e261a831d761fa04f84fab636a4
khludy <khludy3@gmail.com>
2022-09-09 01:30:15 -0700

    Task 2-8

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,18 +2,16 @@
 
 /**
  *
- *  * main - entry point
+ * main - entry point
  *
- *   * Return: 0
+ * Return: 0
  *
- *    */
+ */
 
 int main(void)
 
 {
-
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-
-			return (0);
+	 printf("with proper grammar, but the outcome is a piece of art,\n");
+	 return (0);
 
 }
