d78c9497aa14da5a4c6367e55cb58e5a78744882
khludy <khludy3@gmail.com>
2022-09-12 06:36:15 -0700

    Task 4-8

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -14,15 +14,15 @@ while (lw <= 'z')
 {
 if (lw == 'e' || lw == 'q')
 {
-lw = +1
+lw = +1;
 }
 else
 {
 putchar(lw);
-lw = +1
+lw = +1;
 }
 {
-putchar('\n')
-return(0)
+putchar('\n');
+return(0);
 }
 
