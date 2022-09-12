b0b0f43f926093d825490dd80c7357e27ef6cf26
khludy <khludy3@gmail.com>
2022-09-12 12:44:06 -0700

    Task 7-9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,22 +1,25 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 /**
- * main - entry point
- *
- *  Return: Always 0 (success)
+ * main - printing numbers from 0-9 with commas and space between them
+ * Description: using the main function
+ * this program prints "0, 1, 2, 3, 4, 5, 6, 7, 8, 9"
+ * Return: 0
  */
 
 int main(void)
 {
-int d;
-char low;
+int c;
 
-for (d = '0'; d <= '9'; d++)
-putchar(d);
-for (low = 'a'; low <= 'f'; low++)
-putchar(low);
+for (c = 48; c <= 57; c++)
+{
+putchar(c);
+if (c != 57)
+{
+putchar(',');
+putchar(' ');
+}
+}
 putchar('\n');
 return (0);
 }
