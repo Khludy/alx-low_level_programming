59609361ae6ce4a5efd370ea5e1c1a1a8c9213b1
khludy <khludy3@gmail.com>
2022-09-15 06:35:01 -0700

    Task 0-3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - a function that prints the numbers, from 0 to 9
+ * Returns: returns nothin
+ */
+
+void print_numbers(void);
+{
+int n;
+
+for (n = 48; n < 58; n++)
+{
+putchar(n);
+}
+putchar(10);
+}
