a30bab9af1df29a879e73c292eb2662288cbe659
khludy <khludy3@gmail.com>
2022-09-19 07:18:40 -0700

    Task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,14 +7,15 @@
 
 void puts_half(char *str);
 {
-	int x, y , i;
+	int x, y, i;
 
-	x = strlen(str);
-	if (x % 2 == 1);
-	y = x / 2 + 1;
-	else
-		y = x / 2;
-	for (i = y; i < x; i++)
-		putchar(str[i]);
-	putchar('\n')
-		
+x = strlen(str);
+
+if
+	(x % 2 == 1);
+y = x / 2 + 1;
+else
+y = x / 2;
+for (i = y; i < x; i++)
+putchar(str[i]);
+putchar('\n')
