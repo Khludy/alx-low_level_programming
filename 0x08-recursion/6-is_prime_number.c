0cffdb434879434c9916ef24cee8700442201df0
khludy <khludy3@gmail.com>
2022-09-29 00:51:11 -0700

    Task 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * is _prime_number -finds out if n is prime
+ * @n: number
+ * Return: result
+ */
+
+int is_prime_number(int n)
+{
+	int tmp = 1;
+
+	if (tmp >= n)
+		return (0);
+	if (n % tmp == 0)
+		return (1);
+
+	return (is_prime_number(n % (tmp + 1)));
+}
