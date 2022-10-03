e186f91c03e06fe9d6a27c8a517528cd17c015c7
khludy <khludy3@gmail.com>
2022-10-02 23:19:39 -0700

    Task 1-5

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,45 +1,33 @@
 #include <stdio.h>
-#include <ctype.h>
 #include <stdlib.h>
-#include <string.h>
+#include <ctype.h>
 
 /**
- * checker - checks for valid input
- * @argc: argument count
- * @i: counter for argv[]
- * @j: counter for argv[][]
- * @argv: argument vector
- * Return: 0 on success, 1 on failure
- */
-int checker(int argc, int i, unsigned int j, char *argv[])
-{
-
-for (i = 1; i <= argc; i++)
-for (j = 0; argv[i] != '\0' && j < strlen(argv[i]); j++)
-if (isdigit(argv[i][j]) == 0)
-return (1);
-return (0);
-}
-
-/**
- * main - adds all arguments together if they are digits.
- * @argc: argument count only accepts ints separated by spaces.
- * @argv: argument vector
- * Return: 0 on success, 1 on failure
+ * main - adds numbers
+ * @argc: number of arguments passed to the function
+ * @argv: argument vector of pointers to strin
+ * Return: 0 if no errors, else 1
  */
 
 int main(int argc, char *argv[])
 {
-int result, i;
+int a = 0, i, j;
 
-result = 0;
-if (checker(argc, 1, 0, argv) == 1)
+for (i = 1; i < argc; i++)
 {
-printf("Error\n");
+for (j = 0; argv[i][j]; j++)
+{
+if (isdigit(argv[i][j]) == 0)
+{
+puts("Error");
 return (1);
 }
+}
+}
 for (i = 1; i < argc; i++)
-result += atoi(argv[i]);
-printf("%d\n", result);
+{
+a += atoi(argv[i]);
+}
+printf("%d\n", a);
 return (0);
 }
