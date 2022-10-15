3ffd41cba96b95f333941a222eec4b859e7075fc
khludy <khludy3@gmail.com>
2022-10-15 00:43:58 -0700

    all

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,33 +1,20 @@
-fndef VARIADIC_FUNCTIONS_H
-
+#ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
-
-
-/*
- *
- *  * File: variadic_functions.h
- *
- *   * Auth: Tolulope Fakunle
- *
- *    * Desc: Header file containing prototypes for all functions
- *
- *     *       used in the 0x0F-variadic_functions directory.
- *
- *      */
-
-
+/**
+ * File: variadic_functions.h
+ * Auth: Tolulope Fakunle
+ * Desc: Header file containing prototypes for all functions
+ * used in the 0x0F-variadic_functions directory.
+ */
 
 #include <stdarg.h>
-
-
 
 /**
  * struct printer - A new struct type defining a printer.
  * @symbol: A symbol representing a data type.
  * @print: A function pointer to a function that prints
  * a data type corresponding to symbol.
- *
  */
 
 typedef struct printer
@@ -41,4 +28,4 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
-#endf
+#endif
