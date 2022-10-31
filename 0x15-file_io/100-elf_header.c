c1a36165b549102008140da3ffb034ca61720067
khludy <khludy3@gmail.com>
2022-10-31 04:54:35 -0700

    all

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -122,7 +122,7 @@ void print_version(unsigned char *e_ident)
 {
 	printf("  Version:                           %d",
 			e_ident[EI_VERSION]);
-	
+
 	switch (e_ident[EI_VERSION])
 	{
 		case EV_CURRENT:
